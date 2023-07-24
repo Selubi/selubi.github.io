b5e06f342e1fdb0612eeb187ff18dd448092a688
Selubi <Selubi@users.noreply.github.com>
2023-07-24 02:30:59 +0000

    deploy: 1aeea143f18232ad1ca4ec5a45815ea0e90ab0b3

--- assets/js/fef7f37f.2506820b.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkselubi_tech=self.webpackChunkselubi_tech||[]).push([[248],{6987:e=>{e.exports=JSON.parse('{"label":"Operating Systen","permalink":"/tags/operating-systen","allTagsPath":"/tags","count":1,"items":[{"id":"setup/config/setup-files-iterm","title":"iTerm2 Config Files","description":"","permalink":"/setup/config/setup-files-iterm"}]}')}}]);
